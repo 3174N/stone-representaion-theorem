ee1181abe2b1ccb851f2e79254068775a5f1c881
Eitan Levy <eitan.levy.159@gmail.com>
2025-12-30 17:46:56 +0200

    Add some lemmas

--- Stone/01.lean
+++ Stone/01.lean
@@ -1,40 +1,41 @@
 import Mathlib.Tactic
+import Mathlib.Order.Category.BoolAlg
 import Mathlib.Order.Ideal
 
-variable {A : Type*} [BooleanAlgebra A]
+-- variable {A : Type*} [BoolAlg]
 
-structure BooleanAlgebraHom (α β : Type*) [BooleanAlgebra α] [BooleanAlgebra β]
- extends BoundedLatticeHom α β where
-  map_comp' (a : α) : toFun aᶜ = (toFun a)ᶜ
+lemma non_top_principal_is_proper (A : BoolAlg) (p : A) (hPNonTop : p ≠ ⊤)
+  : (Order.Ideal.IsProper (Order.Ideal.principal p)) := by
+  rw [@Order.Ideal.isProper_iff]
+  rw [Set.ne_univ_iff_exists_notMem]
+  let I := Order.Ideal.principal p
+  have hTopNinI : ⊤ ∉ I := by
+    by_contra!
+    have hTopPEqTop : ⊤ ⊔ p = ⊤ := by exact top_sup_eq p
+    have hTopPEqP : ⊤ ⊔ p = p := by exact sup_of_le_right this
+    have hTIsP : ⊤ = p := by
+      rw [←hTopPEqTop, hTopPEqP]
+    have hPIsTop : p = ⊤ := by exact id (Eq.symm hTIsP)
+    exact hPNonTop hPIsTop
+  use ⊤
+  exact hTopNinI
 
-lemma exists_maximal_ideal (I : Order.Ideal A) :
-  ∃ I' : Order.Ideal A, I'.IsMaximal ∧ I ≤ I' := by
+lemma comp_non_bot_is_non_top (A : BoolAlg) (p : A) (hPNonBot : p ≠ ⊥) : pᶜ ≠ ⊤ := by
+  by_contra!
+  have : p = ⊥ := by exact compl_eq_top.mp this
+  exact hPNonBot this
+
+lemma exists_maximal_ideal (A : BoolAlg) (I : Order.Ideal A) (hIProper : I.IsProper)
+  : ∃ I' : Order.Ideal A, I'.IsProper ∧ I'.IsMaximal ∧ I ≤ I' := by
   sorry
 
-lemma nonzero_homomorphism (p : A) (pNonZero : p ≠ ⊥) :
-  ∃ φ : BooleanAlgebraHom A Bool, φ p = ⊤ := by
+
+lemma nonzero_homomorphism (A : BoolAlg) (B : BoolAlg) (p : A) (pNonZero : p ≠ ⊥) :
+  ∃ φ : A ⟶ B , φ p = ⊤ := by
   let I := Order.Ideal.principal pᶜ
-  have hExMax : ∃ I' : Order.Ideal A, I'.IsMaximal ∧ I ≤ I' := by exact exists_maximal_ideal I
-  obtain ⟨ I', hI' ⟩ := hExMax
-  classical
-  let φ : BooleanAlgebraHom A Bool := {
-    toFun := fun a ↦ if a ∈ I' then ⊥ else ⊤
+  have : pᶜ ≠ ⊤ := comp_non_bot_is_non_top A p pNonZero
+  have IProper : I.IsProper := non_top_principal_is_proper A pᶜ this
 
-    map_sup' := by
-      sorry
-
-    map_inf' := by
-      sorry
-
-    map_comp' := by
-      sorry
-
-    map_bot' := by
-      sorry
-
-    map_top' := by
-      sorry
-  }
-  use φ
-  have : pᶜ ∈ I' := by sorry
+  let φ : BoundedLatticeHom A B := sorry
+  use BoolAlg.ofHom φ
   sorry
