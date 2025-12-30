e88f4c1aba320ef650c7fb60c9e66769549f9b82
Eitan Levy <eitan.levy.159@gmail.com>
2025-12-30 13:44:01 +0200

    Add lake

--- Stone/01.lean
+++ Stone/01.lean
@@ -3,54 +3,30 @@ import Mathlib.Order.Ideal
 
 variable {A : Type*} [BooleanAlgebra A]
 
-lemma exists_maximal_ideal (I : Order.Ideal A):
+structure BooleanAlgebraHom (α β : Type*) [BooleanAlgebra α] [BooleanAlgebra β]
+ extends BoundedLatticeHom α β where
+  map_comp' (a : α) : toFun aᶜ = (toFun a)ᶜ
+
+lemma exists_maximal_ideal (I : Order.Ideal A) :
   ∃ I' : Order.Ideal A, I'.IsMaximal ∧ I ≤ I' := by
   sorry
 
-lemma nonzero_homomorphism (p : A) (pNonZero : p ≠ ⊥):
-  ∃ φ : BoundedLatticeHom A Bool, φ p = ⊤ := by
-  let I : Order.Ideal A := {
-    carrier := { b | b ≤ p }
-
-    nonempty' := by
-      use ⊥
-      have botLeP : ⊥ ≤ p := by exact OrderBot.bot_le p
-      exact botLeP
-
-    lower' := by
-      unfold IsLowerSet
-      intro a b hblea hainI
-      have hblep : b ≤ p := by exact Std.IsPreorder.le_trans b a p hblea hainI
-      exact hblep
-
-    directed' := by
-      unfold DirectedOn
-      intro x hxI y hyI
-      use x ⊔ y
-      constructor
-      · have hxylep : x ⊔ y ≤ p := by exact sup_le hxI hyI
-        exact hxylep
-      · exact ⟨ le_sup_left, le_sup_right ⟩
-  }
+lemma nonzero_homomorphism (p : A) (pNonZero : p ≠ ⊥) :
+  ∃ φ : BooleanAlgebraHom A Bool, φ p = ⊤ := by
+  let I := Order.Ideal.principal pᶜ
   have hExMax : ∃ I' : Order.Ideal A, I'.IsMaximal ∧ I ≤ I' := by exact exists_maximal_ideal I
   obtain ⟨ I', hI' ⟩ := hExMax
   classical
-  let φ : BoundedLatticeHom A Bool := {
-    toFun := fun a ↦ if a ∈ I' then true else false
+  let φ : BooleanAlgebraHom A Bool := {
+    toFun := fun a ↦ if a ∈ I' then ⊥ else ⊤
 
     map_sup' := by
-      intro a b
-      split
-      case isTrue h =>
-        have habInI : a ∈ I' ∧ b ∈ I' := by exact Order.Ideal.sup_mem_iff.mp h
-        simp only [Bool.if_false_right, Bool.and_true, Bool.max_eq_or, Bool.true_eq,
-          Bool.or_eq_true, decide_eq_true_eq]
-        left
-        exact habInI.left
-      case isFalse h =>
-        sorry
+      sorry
 
     map_inf' := by
+      sorry
+
+    map_comp' := by
       sorry
 
     map_bot' := by
@@ -60,5 +36,5 @@ lemma nonzero_homomorphism (p : A) (pNonZero : p ≠ ⊥):
       sorry
   }
   use φ
-  have hpInI : p ∈ I := by exact Order.Ideal.principal_le_iff.mp fun ⦃a⦄ a_1 ↦ a_1
-  aesop
+  have : pᶜ ∈ I' := by sorry
+  sorry
